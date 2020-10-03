3a35e14fa4ead3a497d2172d1ff2dba026613ffe
Brian Faust <hello@basecode.sh>
2020-10-03 05:06:55 +0300

    add ConvertPreSpanToPreCodeModifier

---
+++ app/Modifiers/ConvertPreSpanToPreCodeModifier.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Modifiers;
+
+use App\Contracts\Modifier;
+
+class ConvertPreSpanToPreCodeModifier implements Modifier
+{
+    public function modify(string $content, array $article): string
+    {
+        $content = str_replace('<pre><span>', '<pre><code class="language-javascript">', $content);
+        $content = str_replace('</span><pre>', '</code></pre>', $content);
+
+        return $content;
+    }
+}
