577010679f1b732fb2d165cd2b0b30a4b1d48557
Brian Faust <hello@basecode.sh>
2020-10-04 05:43:15 +0300

    add code highlight wrapper

--- app/Modifiers/ConvertPreSpanToPreCodeModifier.php
+++ app/Modifiers/ConvertPreSpanToPreCodeModifier.php
@@ -8,8 +8,13 @@ class ConvertPreSpanToPreCodeModifier implements Modifier
 {
     public function modify(string $content, array $article): string
     {
-        $content = str_replace('<pre><span>', '<pre><code class="language-javascript">', $content);
-        $content = str_replace('</span></pre>', '</code></pre>', $content);
+        $content = str_replace(
+            '<pre><span>',
+            '<div class="p-4 mb-6 rounded-lg bg-theme-secondary-800 overflow-x-auto"><pre class="hljs"><code class="hljs-copy language-javascript">',
+            $content
+        );
+
+        $content = str_replace('</span></pre>', '</code></pre></div>', $content);
 
         return $content;
     }
